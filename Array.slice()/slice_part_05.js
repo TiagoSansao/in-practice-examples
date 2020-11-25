97321c33894971646de0a2507da56ec8f3fbf0bd
Tiago Sansao <tiagossansao@gmail.com>
2020-11-25 12:37:12 -0300

    Added better explanation

--- Array.slice()/slice_part_05.js
+++ Array.slice()/slice_part_05.js
@@ -2,7 +2,7 @@
 
 const colors = ['red', 'blue', 'green', 'yellow', 'brown', 'purple'];
 
-// in this case slice will copy from the given index and finish two values before the end of the array;
+/* in this case .slice() will copy from the given index and finish two values before the end of the array, you can think it as array.length - (second argument) */
 const copy = colors.slice(1, -2);
 
 console.log(copy);
